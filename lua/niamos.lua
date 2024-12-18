7d5cbf6d5b5f7bd73e745c0c519c66488c37a1c1
hrdm <hrdmilton@gmail.com>
2024-12-18 19:32:57 +0100

    fix(*): snacks and hover

--- lua/niamos.lua
+++ lua/niamos.lua
@@ -769,13 +769,14 @@ function M.set_colors()
 		Group.new("AlphaShortcut", colors.indian_exp_grey_, colors.none, styles.none)
 		Group.new("AlphaFooter", colors.indian_red, colors.none, styles.none)
 		-- snacks
-		Group.new("SnacksDashboardHeader", colors.indian_exp_green:dark(), colors.none, styles.bold)
+		Group.new("SnacksDashboardHeader", colors.indian_green:dark(), colors.none, styles.bold)
 		Group.new("SnacksDashboardTitle", colors.indian_green, colors.none, styles.bold)
-		Group.new("SnacksDashboardIcon", colors.indian_exp_green, colors.none, styles.none)
+		Group.new("SnacksDashboardIcon", colors.indian_green:dark(), colors.none, styles.none)
 		Group.new("SnacksDashboardDesc", colors.indian_blue:light(), colors.none, styles.none)
 		Group.new("SnacksDashboardKey", colors.indian_grey, colors.none, styles.none)
 		-- saga
-		Group.new("lspsagahoverborder", colors.indian_dark_orange, colors.none, styles.none)
+		-- Group.new("HoverNormal", colors.indian_dark_orange, colors.none, styles.none)
+		Group.new("HoverBorder", colors.indian_dark_orange, colors.none, styles.none)
 	end
 
 	function M.translate(group)
