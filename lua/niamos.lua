d19af00b3fb57348a2062e6ae88a291e997ead42
hrdm <hrdmilton@gmail.com>
2024-12-18 02:39:22 +0100

    feat(*): alpha theme

--- lua/niamos.lua
+++ lua/niamos.lua
@@ -762,6 +762,7 @@ function M.set_colors()
 		Group.new("NoiceCmdlineIconSearch", colors.white, colors.indian_red, styles.none)
 		Group.new("NoiceCmdlineIconCmdline", colors.white, colors.indian_green, styles.none)
 		Group.new("NoiceCursor", colors.none, colors.indian_dark_green_3, styles.none)
+		Group.new("AlphaFooter", colors.indian_red, colors.none, colors.none)
 	end
 
 	function M.translate(group)
